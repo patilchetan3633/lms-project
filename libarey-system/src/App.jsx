3288c425a401be1243077ca54a0b29b34767da67
khatrisnehal <khatrisnehal1803@gmail.com>
2025-09-07 12:59:57 +0530

    Update App.jsx

--- libarey-system/src/App.jsx
+++ libarey-system/src/App.jsx
@@ -1,48 +1,31 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
-// import LibraryNavbar from "./Components/Navbar";
-// import { Route, Routes } from "react-router-dom";
-// import Members from "./Components/Member";
-// import Books from "./Components/Books";
-// // import Books from "./Components/Books";
-
-// function App() {
-//   return (
-//     <div>
-//       {/* <BrowserRouter> */}
-//       <LibraryNavbar />
-//       <Routes>
-//         {/* <Route path="/" element={""} /> */}
-//         <Route path="/" element={<Books />} />
-//         <Route path="/Members" element={<Members />} />
-//         <Route path="*" element={<h1>Page-Note-Found</h1>} />
-//       </Routes>
-//       {/* </BrowserRouter> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LibraryNavbar from "./components/Navbar";
-
-import Books from "./Components/Books";
-import Members from "./Components/Member";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import LibraryNavbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Members from "./Components/Members/Member";
+import Books from "./Components/Books/Books";
 
 
 function App() {
+  
   return (
-    <Router>
+    <div>
+      {/* <BrowserRouter> */}
       <LibraryNavbar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/members" element={<Members />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        {/* <Route path="/" element={""} /> */}
+        <Route path="/" element={<Books />} />
+        <Route path="/Members" element={<Members />} />
+        <Route path="*" element={<h1>Page-Note-Found</h1>} />
+      </Routes>
+      {/* </BrowserRouter> */}
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
